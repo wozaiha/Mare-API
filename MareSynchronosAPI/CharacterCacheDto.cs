96d17b006e58614228f00a361b60f569b6e99445
Stanley Dimant <root.darkarchon@outlook.com>
2022-07-24 00:41:44 +0200

    adjust character cache dto

--- MareSynchronosAPI/CharacterCacheDto.cs
+++ MareSynchronosAPI/CharacterCacheDto.cs
@@ -4,9 +4,8 @@ namespace MareSynchronos.API
 {
     public record CharacterCacheDto
     {
-        public ObjectKind ObjectKind { get; set; }
-        public List<FileReplacementDto> FileReplacements { get; set; } = new();
-        public string GlamourerData { get; set; }
+        public Dictionary<ObjectKind, FileReplacementDto> FileReplacements { get; set; } = new();
+        public Dictionary<ObjectKind, string> GlamourerData { get; set; }
         public string ManipulationData { get; set; }
         public string Hash { get; set; }
     }

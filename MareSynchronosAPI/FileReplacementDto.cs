91143894e5314670595c9fe3f364c27280fa2391
Stanley Dimant <root.darkarchon@outlook.com>
2022-07-24 12:22:29 +0200

    add gethashcode to filereplacementdto

--- MareSynchronosAPI/FileReplacementDto.cs
+++ MareSynchronosAPI/FileReplacementDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MareSynchronos.API
 {
@@ -6,5 +7,21 @@ namespace MareSynchronos.API
     {
         public string[] GamePaths { get; set; } = Array.Empty<string>();
         public string Hash { get; set; }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(GetOrderIndependentHashCode(GamePaths), Hash);
+        }
+
+        public static int GetOrderIndependentHashCode<T>(IEnumerable<T> source)
+        {
+            int hash = 0;
+            foreach (T element in source)
+            {
+                hash = unchecked(hash +
+                    EqualityComparer<T>.Default.GetHashCode(element));
+            }
+            return hash;
+        }
     }
 }

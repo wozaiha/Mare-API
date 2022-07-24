e67c399f4e54ea766c860b4ec0c53b5033af616d
Stanley Dimant <root.darkarchon@outlook.com>
2022-07-24 03:25:38 +0200

    add init for cache dto

--- MareSynchronosAPI/CharacterCacheDto.cs
+++ MareSynchronosAPI/CharacterCacheDto.cs
@@ -5,8 +5,8 @@ namespace MareSynchronos.API
     public record CharacterCacheDto
     {
         public Dictionary<ObjectKind, List<FileReplacementDto>> FileReplacements { get; set; } = new();
-        public Dictionary<ObjectKind, string> GlamourerData { get; set; }
-        public string ManipulationData { get; set; }
-        public string Hash { get; set; }
+        public Dictionary<ObjectKind, string> GlamourerData { get; set; } = new();
+        public string ManipulationData { get; set; } = string.Empty;
+        public string Hash { get; set; } = string.Empty;
     }
 }

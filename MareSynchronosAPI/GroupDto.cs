008f88674347b5230b860366017eb3ec157b2090
Stanley Dimant <root.darkarchon@outlook.com>
2022-09-29 15:57:16 +0200

    update api

--- MareSynchronosAPI/GroupDto.cs
+++ MareSynchronosAPI/GroupDto.cs
@@ -3,10 +3,10 @@
     public record GroupDto
     {
         public string GID { get; set; }
-        public string OwnedBy { get; set; }
-        public bool InvitesEnabled { get; set; }
-        public bool IsPaused { get; set; }
-        public bool IsDeleted { get; set; }
-        public string Alias { get; set; }
+        public string? OwnedBy { get; set; }
+        public bool? InvitesEnabled { get; set; }
+        public bool? IsPaused { get; set; }
+        public bool? IsDeleted { get; set; }
+        public string? Alias { get; set; }
     }
 }
--- MareSynchronosAPI/GroupPairDto.cs
+++ MareSynchronosAPI/GroupPairDto.cs
@@ -4,8 +4,8 @@
     {
         public string GroupGID { get; set; }
         public string UserUID { get; set; }
-        public string UserAlias { get; set; }
-        public bool IsPaused { get; set; }
-        public bool IsRemoved { get; set; }
+        public string? UserAlias { get; set; }
+        public bool? IsPaused { get; set; }
+        public bool? IsRemoved { get; set; }
     }
 }

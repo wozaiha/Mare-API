5869ccac093a9ee0013a4d47b62e202daed66a94
Stanley Dimant <stanley.dimant@varian.com>
2022-10-03 10:10:28 +0200

    add shard name to ui

--- MareSynchronosAPI/ConnectionDto.cs
+++ MareSynchronosAPI/ConnectionDto.cs
@@ -6,5 +6,6 @@
         public bool IsAdmin { get; set; }
         public bool IsModerator { get; set; }
         public string UID { get; set; }
+        public string ShardName { get; set; }
     }
 }

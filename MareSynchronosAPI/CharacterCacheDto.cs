79adac7f844a15cebcfd4800d4a91c27f4a6d40c
Stanley Dimant <stanley.dimant@varian.com>
2023-01-13 17:04:39 +0100

    add some initial test impl

--- MareSynchronosAPI/CharacterCacheDto.cs
+++ MareSynchronosAPI/CharacterCacheDto.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace MareSynchronos.API
 {
     public class CharacterCacheDto
     {
+        [JsonIgnore]
+        public string Hash => _hash.Value;
+        [JsonIgnore]
+        private readonly Lazy<string> _hash;
+
+        public CharacterCacheDto()
+        {
+            _hash = new Lazy<string>(() =>
+            {
+                return BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(JsonSerializer.Serialize(this)))).Replace("-", "", StringComparison.OrdinalIgnoreCase);
+            });
+        }
+
         public Dictionary<ObjectKind, List<FileReplacementDto>> FileReplacements { get; set; } = new();
         public Dictionary<ObjectKind, string> GlamourerData { get; set; } = new();
         public string ManipulationData { get; set; } = string.Empty;
         public string CustomizePlusData { get; set; } = string.Empty;
+        public float HeelsOffset { get; set; } = 0.0f;
+
         public override string ToString()
         {
             return GetHashCode() + Environment.NewLine + "Manip:" + ManipulationData + Environment.NewLine
@@ -17,8 +36,6 @@ namespace MareSynchronos.API
                 + string.Join(Environment.NewLine, FileReplacements.Select(g => g.Key + Environment.NewLine + string.Join(Environment.NewLine, g.Value)))
                 + Environment.NewLine + "CustomizePlus:" + CustomizePlusData;
         }
-
-        public float HeelsOffset { get; set; } = 0.0f;
 
         public static int GetOrderIndependentHashCode<T>(IEnumerable<T> source)
         {

473abee6aa46cf80b4c9483b49e8be6e9845d18a
rootdarkarchon <root.darkarchon@outlook.com>
2023-01-26 22:12:34 +0100

    fix extenions

--- MareSynchronosAPI/Data/Extensions/GroupUserPermissionsExtensions.cs
+++ MareSynchronosAPI/Data/Extensions/GroupUserPermissionsExtensions.cs
@@ -19,21 +19,21 @@ public static class GroupUserPermissionsExtensions
         return perm.HasFlag(GroupUserPermissions.Paused);
     }
 
-    public static void SetDisableAnimations(this ref GroupUserPermissions perm, bool disable)
+    public static void SetDisableAnimations(this ref GroupUserPermissions perm, bool set)
     {
-        if (disable) perm &= ~GroupUserPermissions.DisableAnimations;
-        else perm |= GroupUserPermissions.DisableAnimations;
+        if (set) perm |= GroupUserPermissions.DisableAnimations;
+        else perm &= ~GroupUserPermissions.DisableAnimations;
     }
 
-    public static void SetDisableSounds(this ref GroupUserPermissions perm, bool disable)
+    public static void SetDisableSounds(this ref GroupUserPermissions perm, bool set)
     {
-        if (disable) perm &= ~GroupUserPermissions.DisableSounds;
-        else perm |= GroupUserPermissions.DisableSounds;
+        if (set) perm |= GroupUserPermissions.DisableSounds;
+        else perm &= ~GroupUserPermissions.DisableSounds;
     }
 
-    public static void SetPaused(this ref GroupUserPermissions perm, bool disable)
+    public static void SetPaused(this ref GroupUserPermissions perm, bool set)
     {
-        if (disable) perm &= ~GroupUserPermissions.Paused;
-        else perm |= GroupUserPermissions.Paused;
+        if (set) perm |= GroupUserPermissions.Paused;
+        else perm &= ~GroupUserPermissions.Paused;
     }
 }

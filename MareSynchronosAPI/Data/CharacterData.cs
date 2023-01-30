f604e6d27ebf760dd44feb53a11b8c04e91edc67
Cara <git@caraxian.com>
2023-01-30 19:13:40 +1030

    Add PalettePlus supprt (#11)

--- MareSynchronosAPI/Data/CharacterData.cs
+++ MareSynchronosAPI/Data/CharacterData.cs
@@ -8,5 +8,6 @@ public class CharacterData : HashableDataBase
     public Dictionary<ObjectKind, string> GlamourerData { get; set; } = new();
     public string ManipulationData { get; set; } = string.Empty;
     public string CustomizePlusData { get; set; } = string.Empty;
+    public string PalettePlusData { get; set; } = string.Empty;
     public float HeelsOffset { get; set; } = 0.0f;
 }

f8e647af00be85404523e77920c03fc1a9eb894a
rootdarkarchon <root.darkarchon@outlook.com>
2023-03-13 23:37:59 +0100

    add upload status to server

--- MareSynchronosAPI/SignalR/IMareHub.cs
+++ MareSynchronosAPI/SignalR/IMareHub.cs
@@ -10,54 +10,93 @@ public interface IMareHub
     const int ApiVersion = 23;
     const string Path = "/mare";
 
-    Task<ConnectionDto> GetConnectionDto();
     Task<bool> CheckClientHealth();
 
-    Task<List<UserPairDto>> UserGetPairedClients();
+    Task Client_DownloadReady(Guid requestId);
+
+    Task Client_GroupChangePermissions(GroupPermissionDto groupPermission);
+
+    Task Client_GroupDelete(GroupDto groupDto);
+
+    Task Client_GroupPairChangePermissions(GroupPairUserPermissionDto permissionDto);
+
+    Task Client_GroupPairChangeUserInfo(GroupPairUserInfoDto userInfo);
+
+    Task Client_GroupPairJoined(GroupPairFullInfoDto groupPairInfoDto);
+
+    Task Client_GroupPairLeft(GroupPairDto groupPairDto);
+
+    Task Client_GroupSendFullInfo(GroupFullInfoDto groupInfo);
+
+    Task Client_GroupSendInfo(GroupInfoDto groupInfo);
+
+    Task Client_ReceiveServerMessage(MessageSeverity messageSeverity, string message);
+
+    Task Client_UpdateSystemInfo(SystemInfoDto systemInfo);
+
+    Task Client_UserAddClientPair(UserPairDto dto);
+
+    Task Client_UserReceiveCharacterData(OnlineUserCharaDataDto dataDto);
+
+    Task Client_UserReceiveUploadStatus(UserDto dto);
+
+    Task Client_UserRemoveClientPair(UserDto dto);
+
+    Task Client_UserSendOffline(UserDto dto);
+
+    Task Client_UserSendOnline(OnlineUserIdentDto dto);
+
+    Task Client_UserUpdateOtherPairPermissions(UserPermissionsDto dto);
+
+    Task Client_UserUpdateSelfPairPermissions(UserPermissionsDto dto);
+
+    Task<ConnectionDto> GetConnectionDto();
+
+    Task GroupBanUser(GroupPairDto dto, string reason);
+
+    Task GroupChangeGroupPermissionState(GroupPermissionDto dto);
+
+    Task GroupChangeIndividualPermissionState(GroupPairUserPermissionDto dto);
+
+    Task GroupChangeOwnership(GroupPairDto groupPair);
+
+    Task<bool> GroupChangePassword(GroupPasswordDto groupPassword);
+
+    Task GroupClear(GroupDto group);
+
+    Task<GroupPasswordDto> GroupCreate();
+
+    Task<List<string>> GroupCreateTempInvite(GroupDto group, int amount);
+
+    Task GroupDelete(GroupDto group);
+
+    Task<List<BannedGroupUserDto>> GroupGetBannedUsers(GroupDto group);
+
+    Task<bool> GroupJoin(GroupPasswordDto passwordedGroup);
+
+    Task GroupLeave(GroupDto group);
+
+    Task GroupRemoveUser(GroupPairDto groupPair);
+
+    Task GroupSetUserInfo(GroupPairUserInfoDto groupPair);
+
+    Task<List<GroupFullInfoDto>> GroupsGetAll();
+
+    Task<List<GroupPairFullInfoDto>> GroupsGetUsersInGroup(GroupDto group);
+
+    Task GroupUnbanUser(GroupPairDto groupPair);
+
     Task UserAddPair(UserDto user);
-    Task UserRemovePair(UserDto userDto);
-    Task UserSetPairPermissions(UserPermissionsDto userPermissions);
-    Task<List<OnlineUserIdentDto>> UserGetOnlinePairs();
-    Task UserPushData(UserCharaDataMessageDto dto);
 
     Task UserDelete();
 
-    Task<List<BannedGroupUserDto>> GroupGetBannedUsers(GroupDto group);
-    Task GroupClear(GroupDto group);
-    Task GroupChangeOwnership(GroupPairDto groupPair);
-    Task<bool> GroupChangePassword(GroupPasswordDto groupPassword);
-    Task<GroupPasswordDto> GroupCreate();
-    Task<List<GroupFullInfoDto>> GroupsGetAll();
-    Task<List<GroupPairFullInfoDto>> GroupsGetUsersInGroup(GroupDto group);
-    Task GroupBanUser(GroupPairDto dto, string reason);
-    Task GroupChangeGroupPermissionState(GroupPermissionDto dto);
-    Task GroupChangeIndividualPermissionState(GroupPairUserPermissionDto dto);
-    Task GroupDelete(GroupDto group);
-    Task<bool> GroupJoin(GroupPasswordDto passwordedGroup);
-    Task GroupLeave(GroupDto group);
-    Task GroupRemoveUser(GroupPairDto groupPair);
-    Task GroupUnbanUser(GroupPairDto groupPair);
-    Task GroupSetUserInfo(GroupPairUserInfoDto groupPair);
-    Task<List<string>> GroupCreateTempInvite(GroupDto group, int amount);
+    Task<List<OnlineUserIdentDto>> UserGetOnlinePairs();
 
-    Task Client_UpdateSystemInfo(SystemInfoDto systemInfo);
-    Task Client_ReceiveServerMessage(MessageSeverity messageSeverity, string message);
-    Task Client_DownloadReady(Guid requestId);
+    Task<List<UserPairDto>> UserGetPairedClients();
 
-    Task Client_UserSendOnline(OnlineUserIdentDto dto);
-    Task Client_UserSendOffline(UserDto dto);
-    Task Client_UserAddClientPair(UserPairDto dto);
-    Task Client_UserRemoveClientPair(UserDto dto);
-    Task Client_UserUpdateSelfPairPermissions(UserPermissionsDto dto);
-    Task Client_UserUpdateOtherPairPermissions(UserPermissionsDto dto);
-    Task Client_UserReceiveCharacterData(OnlineUserCharaDataDto dataDto);
+    Task UserPushData(UserCharaDataMessageDto dto);
 
-    Task Client_GroupSendFullInfo(GroupFullInfoDto groupInfo);
-    Task Client_GroupSendInfo(GroupInfoDto groupInfo);
-    Task Client_GroupDelete(GroupDto groupDto);
-    Task Client_GroupPairJoined(GroupPairFullInfoDto groupPairInfoDto);
-    Task Client_GroupPairLeft(GroupPairDto groupPairDto);
-    Task Client_GroupChangePermissions(GroupPermissionDto groupPermission);
-    Task Client_GroupPairChangePermissions(GroupPairUserPermissionDto permissionDto);
-    Task Client_GroupPairChangeUserInfo(GroupPairUserInfoDto userInfo);
+    Task UserRemovePair(UserDto userDto);
+
+    Task UserSetPairPermissions(UserPermissionsDto userPermissions);
 }
--- MareSynchronosAPI/SignalR/IMareHubClient.cs
+++ MareSynchronosAPI/SignalR/IMareHubClient.cs
@@ -7,24 +7,41 @@ namespace MareSynchronos.API.SignalR;
 
 public interface IMareHubClient : IMareHub
 {
-    void OnUpdateSystemInfo(Action<SystemInfoDto> act);
-    void OnReceiveServerMessage(Action<MessageSeverity, string> act);
     void OnDownloadReady(Action<Guid> act);
 
-    void OnUserSendOnline(Action<OnlineUserIdentDto> act);
-    void OnUserSendOffline(Action<UserDto> act);
-    void OnUserAddClientPair(Action<UserPairDto> act);
-    void OnUserRemoveClientPair(Action<UserDto> act);
-    void OnUserUpdateSelfPairPermissions(Action<UserPermissionsDto> act);
-    void OnUserUpdateOtherPairPermissions(Action<UserPermissionsDto> act);
-    void OnUserReceiveCharacterData(Action<OnlineUserCharaDataDto> act);
+    void OnGroupChangePermissions(Action<GroupPermissionDto> act);
+
+    void OnGroupDelete(Action<GroupDto> act);
+
+    void OnGroupPairChangePermissions(Action<GroupPairUserPermissionDto> act);
+
+    void OnGroupPairChangeUserInfo(Action<GroupPairUserInfoDto> act);
+
+    void OnGroupPairJoined(Action<GroupPairFullInfoDto> act);
+
+    void OnGroupPairLeft(Action<GroupPairDto> act);
 
     void OnGroupSendFullInfo(Action<GroupFullInfoDto> act);
+
     void OnGroupSendInfo(Action<GroupInfoDto> act);
-    void OnGroupDelete(Action<GroupDto> act);
-    void OnGroupPairJoined(Action<GroupPairFullInfoDto> act);
-    void OnGroupPairLeft(Action<GroupPairDto> act);
-    void OnGroupChangePermissions(Action<GroupPermissionDto> act);
-    void OnGroupPairChangePermissions(Action<GroupPairUserPermissionDto> act);
-    void OnGroupPairChangeUserInfo(Action<GroupPairUserInfoDto> act);
+
+    void OnReceiveServerMessage(Action<MessageSeverity, string> act);
+
+    void OnUpdateSystemInfo(Action<SystemInfoDto> act);
+
+    void OnUserAddClientPair(Action<UserPairDto> act);
+
+    void OnUserReceiveCharacterData(Action<OnlineUserCharaDataDto> act);
+
+    void OnUserReceiveUploadStatus(Action<UserDto> act);
+
+    void OnUserRemoveClientPair(Action<UserDto> act);
+
+    void OnUserSendOffline(Action<UserDto> act);
+
+    void OnUserSendOnline(Action<OnlineUserIdentDto> act);
+
+    void OnUserUpdateOtherPairPermissions(Action<UserPermissionsDto> act);
+
+    void OnUserUpdateSelfPairPermissions(Action<UserPermissionsDto> act);
 }

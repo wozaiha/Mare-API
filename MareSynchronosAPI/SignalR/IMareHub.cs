7742d2bf4dadf39427e77e646c437dbaafc28728
rootdarkarchon <root.darkarchon@outlook.com>
2023-08-19 15:27:15 +0200

    bump api version

--- MareSynchronosAPI/SignalR/IMareHub.cs
+++ MareSynchronosAPI/SignalR/IMareHub.cs
@@ -7,7 +7,7 @@ namespace MareSynchronos.API.SignalR;
 
 public interface IMareHub
 {
-    const int ApiVersion = 26;
+    const int ApiVersion = 27;
     const string Path = "/mare";
 
     Task<bool> CheckClientHealth();

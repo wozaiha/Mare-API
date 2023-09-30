999679cc2d7ee6733ca14961493fa652d732c491
rootdarkarchon <root.darkarchon@outlook.com>
2023-09-30 13:36:14 +0200

    update api

--- MareSynchronosAPI/SignalR/IMareHubClient.cs
+++ MareSynchronosAPI/SignalR/IMareHubClient.cs
@@ -13,8 +13,6 @@ public interface IMareHubClient : IMareHub
 
     void OnGroupDelete(Action<GroupDto> act);
 
-    void OnGroupPairChangePermissions(Action<GroupPairUserPermissionDto> act);
-
     void OnGroupPairChangeUserInfo(Action<GroupPairUserInfoDto> act);
 
     void OnGroupPairJoined(Action<GroupPairFullInfoDto> act);
@@ -46,5 +44,6 @@ public interface IMareHubClient : IMareHub
     void OnUserUpdateProfile(Action<UserDto> act);
 
     void OnUserUpdateSelfPairPermissions(Action<UserPermissionsDto> act);
+
     void OnUserDefaultPermissionUpdate(Action<DefaultPermissionsDto> act);
 }

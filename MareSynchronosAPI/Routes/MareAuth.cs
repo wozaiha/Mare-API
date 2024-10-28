c1e62b3ed133a9e70b69886fe038bf6d3de522c6
Stanley Dimant <root.darkarchon@outlook.com>
2024-10-29 00:05:08 +0100

    api update

--- MareSynchronosAPI/Routes/MareAuth.cs
+++ MareSynchronosAPI/Routes/MareAuth.cs
@@ -5,7 +5,11 @@ public class MareAuth
 
     public const string Auth = "/auth";
     public const string Auth_CreateIdent = "createWithIdent";
+    public const string Auth_CreateOAuth = "createWithOAuth";
     public const string Auth_RenewToken = "renewToken";
+    public const string Auth_RenewOAuthToken = "renewOAuthToken";
     public static Uri AuthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateIdent);
+    public static Uri AuthWithOauthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateOAuth);
     public static Uri RenewTokenFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_RenewToken);
+    public static Uri RenewOAuthTokenFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_RenewOAuthToken);
 }

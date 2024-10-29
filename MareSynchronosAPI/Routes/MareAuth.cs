93fc431391c9980ebc4cdbfd55780ef095d59bbb
Stanley Dimant <root.darkarchon@outlook.com>
2024-10-29 10:08:50 +0100

    add and adjust more routes

--- MareSynchronosAPI/Routes/MareAuth.cs
+++ MareSynchronosAPI/Routes/MareAuth.cs
@@ -2,14 +2,20 @@
 
 public class MareAuth
 {
-
+    public const string OAuth = "/oauth";
     public const string Auth = "/auth";
     public const string Auth_CreateIdent = "createWithIdent";
-    public const string Auth_CreateOAuth = "createWithOAuth";
     public const string Auth_RenewToken = "renewToken";
-    public const string Auth_RenewOAuthToken = "renewOAuthToken";
+    public const string OAuth_CreateOAuth = "createWithOAuth";
+    public const string OAuth_RenewOAuthToken = "renewToken";
+    public const string OAuth_GetDiscordOAuthEndpoint = "getDiscordOAuthEndpoint";
+    public const string OAuth_GetUIDs = "getUIDs";
+    public const string OAuth_GetDiscordOAuthToken = "getDiscordOAuthToken";
     public static Uri AuthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateIdent);
-    public static Uri AuthWithOauthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateOAuth);
+    public static Uri AuthWithOauthFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_CreateOAuth);
     public static Uri RenewTokenFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_RenewToken);
-    public static Uri RenewOAuthTokenFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_RenewOAuthToken);
+    public static Uri RenewOAuthTokenFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_RenewOAuthToken);
+    public static Uri GetDiscordOAuthEndpoint(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthEndpoint);
+    public static Uri GetDiscordOAuthToken(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthToken);
+    public static Uri GetUIDs(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetUIDs);
 }

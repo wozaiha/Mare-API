dc1c409ec65934fe3c50889d0517bc68f5fda431
Stanley Dimant <root.darkarchon@outlook.com>
2024-10-31 09:25:59 +0100

    add api call to get uids based on secret key, renaming

--- MareSynchronosAPI/Routes/MareAuth.cs
+++ MareSynchronosAPI/Routes/MareAuth.cs
@@ -6,6 +6,7 @@ public class MareAuth
     public const string Auth = "/auth";
     public const string Auth_CreateIdent = "createWithIdent";
     public const string Auth_RenewToken = "renewToken";
+    public const string OAuth_GetUIDsBasedOnSecretKeys = "getUIDsViaSecretKey";
     public const string OAuth_CreateOAuth = "createWithOAuth";
     public const string OAuth_RenewOAuthToken = "renewToken";
     public const string OAuth_GetDiscordOAuthEndpoint = "getDiscordOAuthEndpoint";
@@ -15,7 +16,8 @@ public class MareAuth
     public static Uri AuthWithOauthFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_CreateOAuth);
     public static Uri RenewTokenFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_RenewToken);
     public static Uri RenewOAuthTokenFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_RenewOAuthToken);
-    public static Uri GetDiscordOAuthEndpoint(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthEndpoint);
-    public static Uri GetDiscordOAuthToken(Uri baseUri, string sessionId) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthToken + "?sessionId=" + sessionId);
-    public static Uri GetUIDs(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetUIDs);
+    public static Uri GetUIDsBasedOnSecretKeyFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetUIDsBasedOnSecretKeys);
+    public static Uri GetDiscordOAuthEndpointFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthEndpoint);
+    public static Uri GetDiscordOAuthTokenFullPath(Uri baseUri, string sessionId) => new Uri(baseUri, OAuth + "/" + OAuth_GetDiscordOAuthToken + "?sessionId=" + sessionId);
+    public static Uri GetUIDsFullPath(Uri baseUri) => new Uri(baseUri, OAuth + "/" + OAuth_GetUIDs);
 }

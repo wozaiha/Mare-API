a02e85e2f61824340d7f2b594affce1f117dab25
Tsubasahane <wozaiha@gmail.com>
2024-12-11 16:43:41 +0800

    Moodles Dto

---
+++ MareSynchronosAPI/Dto/User/ApplyMoodlesByStatusDto.cs
@@ -0,0 +1,7 @@
+﻿using MareSynchronos.API.Data;
+using MessagePack;
+
+namespace MareSynchronos.API.Dto.User;
+
+[MessagePackObject(keyAsPropertyName: true)]
+public record ApplyMoodlesByStatusDto(UserData User, List<MoodlesStatusInfo> Statuses, MoodlesIpcToggleType Type) : UserDto(User);

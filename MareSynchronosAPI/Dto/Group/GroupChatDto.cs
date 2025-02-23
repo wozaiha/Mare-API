e8b4b278ed34d5d31276f6868bcdce1934afb5a7
Tsubasahane <wozaiha@gmail.com>
2025-02-23 13:21:57 +0800

    Update GroupChatDto.cs

--- MareSynchronosAPI/Dto/Group/GroupChatDto.cs
+++ MareSynchronosAPI/Dto/Group/GroupChatDto.cs
@@ -4,6 +4,6 @@ using MessagePack;
 namespace MareSynchronos.API.Dto.Group;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record GroupChatDto(UserData User, GroupData Group, string Message) : GroupDto(Group)
+public record GroupChatDto(UserData User, GroupData Group,DateTime Time, string Message) : GroupDto(Group)
 {
 }

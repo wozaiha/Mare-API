a0ee63594757bc360adab697e3f8c42cefc1ffbb
Tsubasahane <wozaiha@gmail.com>
2025-07-09 19:08:13 +0800

    Update PFinderDto.cs

--- MareSynchronosAPI/Dto/Group/PFinderDto.cs
+++ MareSynchronosAPI/Dto/Group/PFinderDto.cs
@@ -4,34 +4,62 @@ using MessagePack;
 namespace MareSynchronos.API.Dto.Group;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record PFinderDto(Guid Guid, DateTime StartTime, DateTime EndTime, DateTime LastUpdate, string Title, string Description, string Tags,
-    bool IsNSFW, bool Open,
-    GroupData Group, UserData User) : GroupDto(Group)
+public record PFinderDto
 {
+    public Guid Guid { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Tags { get; set; }
+    public bool IsNSFW { get; set; }
+    public bool Open { get; set; }
+    public GroupData Group { get; set; }
+    public UserData User { get; set; }
 
-    public Guid Guid { get; set; } = Guid;
+    public PFinderDto(Guid guid, DateTime startTime, DateTime endTime, DateTime lastUpdate, string title, string description, string tags,
+        bool isNSFW, bool open, GroupData group, UserData user)
+    {
+        Guid = guid;
+        StartTime = startTime;
+        EndTime = endTime;
+        LastUpdate = lastUpdate;
+        Title = title;
+        Description = description;
+        Tags = tags;
+        IsNSFW = isNSFW;
+        Open = open;
+        Group = group;
+        User = user;
+    }
 
-    public DateTime StartTime { get; set; } = StartTime;
+    public PFinderDto()
+    {
+        Guid = Guid.NewGuid();
+        StartTime = DateTime.Now;
+        EndTime = DateTime.Now;
+        LastUpdate = DateTime.Now;
+        Title = string.Empty;
+        Description = string.Empty;
+        Tags = string.Empty;
+        IsNSFW = false;
+        Open = false;
+        Group = new GroupData(string.Empty);
+        User = new UserData(string.Empty);
+    }
 
-    public DateTime EndTime { get; set; } = EndTime;
-
-    public DateTime LastUpdate { get; set; } = LastUpdate;
-
-    public string Title { get; set; } = Title;
-
-    public string Description { get; set; } = Description;
-
-    public string Tags { get; set; } = Tags;
-
-    public bool IsNSFW { get; set; } = IsNSFW;
-
-    public bool Open { get; set; } = Open;
-
-    public GroupData Group { get; set; } = Group;
-
-    public UserData User { get; set; } = User;
-
+    public bool IsVaild()
+    {
+        if (Guid == Guid.Empty) return false;
+        if (StartTime < DateTime.Now) return false;
+        if (EndTime < DateTime.Now) return false;
+        if (StartTime + TimeSpan.FromDays(7) > EndTime) return false;
+        if (string.IsNullOrEmpty(Title)) return false;
+        if (string.IsNullOrEmpty(Description)) return false;
+        if (string.IsNullOrEmpty(Tags)) return false;
+        if (string.IsNullOrEmpty(Group.GID)) return false;
+        if (string.IsNullOrEmpty(User.UID)) return false;
+        return true;
+    }
 }
-
-[MessagePackObject(keyAsPropertyName: true)]
-public record PFinderListDto(List<PFinderDto> PFinderList);

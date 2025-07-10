50b2d988a2e48de79333a8a3672aa3762389a152
Tsubasahane <wozaiha@gmail.com>
2025-07-10 16:29:08 +0800

    Update PFinderDto.cs

--- MareSynchronosAPI/Dto/Group/PFinderDto.cs
+++ MareSynchronosAPI/Dto/Group/PFinderDto.cs
@@ -52,9 +52,8 @@ public record PFinderDto
     public bool IsVaild()
     {
         if (Guid == Guid.Empty) return false;
-        if (StartTime > DateTime.Now) return false;
         if (EndTime < DateTime.Now) return false;
-        if (StartTime + TimeSpan.FromDays(7) < EndTime || StartTime > EndTime) return false;
+        if (StartTime + TimeSpan.FromDays(1) < EndTime || StartTime > EndTime) return false;
         if (string.IsNullOrEmpty(Title)) return false;
         if (string.IsNullOrEmpty(Description)) return false;
         if (string.IsNullOrEmpty(Group.GID)) return false;
